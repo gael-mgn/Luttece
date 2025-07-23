da15d967e5e376444a3e392e7b92a678ad200b00
gael-mgn <gael.maignan@eivp-paris.fr>
2025-07-23 09:38:55 +0200

    Add files via upload

---
+++ data/manifestations_interdites_form.js
@@ -0,0 +1,10 @@
+const SHEET_ID = "1HT87wl3molF9l8DuwDZy3opAdEopQ2ekXhH4XfF-GoM"; // à remplacer
+const SHEET_NAME = "Feuille 1"; // ou adapte si ta feuille a un autre nom
+
+const champs = [
+  { name: "age", label: "Âge", type: "number" },
+  { name: "taille", label: "Taille (cm)", type: "number" },
+  { name: "poids", label: "Poids (kg)", type: "number" },
+  { name: "date_mesure", label: "Date de la mesure", type: "date" },
+  { name: "commentaire", label: "Commentaire", type: "text" }
+];
