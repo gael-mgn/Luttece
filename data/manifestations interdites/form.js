64e31a58e3b93891c77dc836075f588adc775d0d
gael-mgn <gael.maignan@eivp-paris.fr>
2025-07-23 07:47:27 +0200

    Add files via upload

---
+++ data/manifestations interdites/form.js
@@ -0,0 +1,7 @@
+const champs = [
+  { name: "age", label: "Âge", type: "number" },
+  { name: "taille", label: "Taille (cm)", type: "number" },
+  { name: "poids", label: "Poids (kg)", type: "number" },
+  { name: "date_mesure", label: "Date de la mesure", type: "date" },
+  { name: "commentaire", label: "Commentaire", type: "text" }
+];
