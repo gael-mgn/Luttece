9071aaa0e752651b9a136f44bf101c71467fbfeb
gael-mgn <gael.maignan@eivp-paris.fr>
2025-07-23 07:50:52 +0200

    Add files via upload

--- data/manifestations interdites/form.js
+++ data/manifestations interdites/form.js
@@ -1,3 +1,6 @@
+const SHEET_ID = "1HT87wl3molF9l8DuwDZy3opAdEopQ2ekXhH4XfF-GoM"; // à remplacer
+const SHEET_NAME = "Feuille 1"; // ou adapte si ta feuille a un autre nom
+
 const champs = [
   { name: "age", label: "Âge", type: "number" },
   { name: "taille", label: "Taille (cm)", type: "number" },
